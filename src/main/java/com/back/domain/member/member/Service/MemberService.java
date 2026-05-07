e592a4d8c7e8fd348d9c463d0a8958a4cb0cf1a5
ktk3376 <ktk3376@gmail.com>
2026-05-07 10:40:48 +0900

    44강, Member, MemberRepository, MemberService 추가

---
+++ src/main/java/com/back/domain/member/member/Service/MemberService.java
@@ -0,0 +1,11 @@
+package com.back.domain.member.member.Service;
+
+import com.back.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+}
